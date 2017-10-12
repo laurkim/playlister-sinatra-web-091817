092be31a307c056bb402fa0fb397bd67e5fff392
laurkim <laurakeem@gmail.com>
2017-10-11 22:34:05 -0400

    all tests passing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
@@ -18,10 +21,11 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(params["song"])
-    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song = Song.create(:name => params["song_name"])
+    @song.artist = Artist.find_or_create_by(:name => params["artist_name"])
     @song.genre_ids = params[:genres]
     @song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
 
@@ -33,10 +37,14 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.save
-
-    redirect("/songs/#{@song.slug}")
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    if @song.save
+      flash[:message] = "Successfully updated song."
+      redirect("/songs/#{@song.slug}")
+    else
+      flash[:message] = "Error."
+      erb :'songs/edit'
+    end
   end
 
 end
